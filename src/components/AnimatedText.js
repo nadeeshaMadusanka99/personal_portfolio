91aa5692e5f82c8df6cd69779c7366e383e5755a
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-01-13 14:02:49 +0530

    Picture and text added on the front page

---
+++ src/components/AnimatedText.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const quote = {
+  initial: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.08,
+    },
+  },
+};
+
+const singleWord = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren:0.08
+    },
+  },
+};
+
+const AnimatedText = ({ text, className = "" }) => {
+    return (
+      <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0">
+        <motion.h1
+          className={`inline-block w-full text-dark font-extrabold capitalize text-8xl ${className}`}
+          variants={quote}
+          initial="initial"
+          animate="animate"
+        >
+          {text.split("\n").map((line, lineIndex) => (
+            <React.Fragment key={line + "" + lineIndex}>
+              {lineIndex > 0 && <br />} 
+              {line.split(" ").map((word, index) => (
+                <motion.span
+                  key={word + "" + index}
+                  className="inline-block"
+                  variants={singleWord}
+                >
+                  {word}&nbsp;
+                </motion.span>
+              ))}
+            </React.Fragment>
+          ))}
+        </motion.h1>
+      </div>
+    );
+  };
+
+export default AnimatedText;
