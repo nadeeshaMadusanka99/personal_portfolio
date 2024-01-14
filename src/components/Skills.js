e0ef3be0b49aff5a595364669718cb2061e4280a
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-01-14 08:29:53 +0530

    Skills background added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
     <>
-        
-    <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
-    <div className='w-full h-screen  flex items-center justify-center rounded-full'>
-
-    </div>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+      <div className="w-full h-screen  flex items-center justify-center rounded-full bg-circularLight">
+        <motion.div
+          className="flex items-center justify-center flex-col rounded-full font-semibold bg-dark text-light px-5 py-3 shadow-dark cursor-pointer"
+          whileHover={{ scale: 1.05 }}
+        >
+          <span className="text-center">
+            Web <br /> & <br /> Mobile
+          </span>
+        </motion.div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
