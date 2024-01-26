17564d1c486b872431999f964716a3700d175551
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-01-26 20:22:05 +0530

    Add resposiveness to Contact me button and nav bar

--- src/components/Common/NavBar.js
+++ src/components/Common/NavBar.js
@@ -80,7 +80,7 @@ const NavBar = () => {
     e.stopPropagation();
   };
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
       <button
         className=" flex-col justify-center items-center hidden lg:flex"
         onClick={(e) => handleClick(e)}
--- src/components/Home/ContactMeIcon.js
+++ src/components/Home/ContactMeIcon.js
@@ -8,12 +8,12 @@ const ContactMeIcon = () => {
   return (
     <div
       className="fixed left-4 bottom-4 flex items-center justify-center 
-    overflow-hidden"
+    overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
+  sm:right-0"
     >
-      <div className="w-48 h-auto flex items-center justify-center relative">
+      <div className="w-48 h-auto flex items-center justify-center relative  md:w-24">
         <CircularText
           className="fill-dark animate-spin-slow dark:fill-light "
-          //   style={{ fontSize: "66px" }}
         />
         <MotionLink
           href="/contact_me"
@@ -21,7 +21,7 @@ const ContactMeIcon = () => {
           -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid
          border-dark w-[85px] h-[85px] rounded-full font-semibold hover:bg-light hover:text-dark
           hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark
-           hover:dark:text-light hover:dark:border-light"
+           hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px]"
         >
           <div className="text-center">
             Contact <br /> Me
