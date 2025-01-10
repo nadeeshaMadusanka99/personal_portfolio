08e281c54f3c67a0bc1ebb74322fdbdeff6ffbcf
Nadeesha Madusanka <nadeeshamadusanka44@gmail.com>
2025-01-10 20:33:29 +0530

    Add animation to pop up the burger menu when scrolling up

--- src/components/Common/NavBar.js
+++ src/components/Common/NavBar.js
@@ -9,7 +9,12 @@ import {
   SunIcon,
   MoonIcon,
 } from "./Icons";
-import { motion } from "framer-motion";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
 import useThemeSwitcher from "../Hooks/useThemeSwitcher";
 
 const CustomLink = ({ href, title, className = "" }) => {
@@ -61,6 +66,25 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const popUP = useRef(null);
 
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(true);
+
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    if (typeof current === "number") {
+      const direction = current - (scrollYProgress.getPrevious() || 0);
+
+      if (scrollYProgress.get() < 0.05) {
+        setVisible(true);
+      } else if (direction < 0) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    }
+  });
+
   useEffect(() => {
     if (!isOpen) return;
     function handleClickOutside(event) {
@@ -69,7 +93,6 @@ const NavBar = () => {
       }
     }
     window.addEventListener("click", handleClickOutside);
-    // clean up
     return () => window.removeEventListener("click", handleClickOutside);
   }, [isOpen]);
 
@@ -78,189 +101,215 @@ const NavBar = () => {
     e.stopPropagation();
   };
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative lg:px-16 md:px-12 sm:px-8 sm:pb-4">
-      <button
-        className=" flex-col justify-centeritems-center hidden lg:flex fixed top-0 z-10 bg-light mt-5 rounded-full px-3 py-5 dark:bg-dark dark:text-light"
-        onClick={(e) => handleClick(e)}
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+        }}
+        exit={{
+          opacity: 0,
+          y: -100,
+          transition: {
+            duration: 0.4,
+            ease: "easeInOut",
+          },
+        }}
       >
-        <span
-          className={`bg-dark dark:bg-light block transition-all duration-400 ease-out h-0.5 w-6 rounded-sm ${isOpen ? `rotate-45 translate-y-1` : `-translate-y-0.5`
-            }`}
-        ></span>
-        <span
-          className={`bg-dark dark:bg-light block transition-all duration-400 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? `opacity-0` : `opacity-100`
-            }`}
-        ></span>
-        <span
-          className={`bg-dark dark:bg-light block transition-all duration-400 ease-out h-0.5 w-6 rounded-sm ${isOpen ? `-rotate-45 -translate-y-1` : `translate-y-0.5`
-            }`}
-        ></span>
-      </button>
 
-      <div className="w-full flex justify-between items-center lg:hidden">
-        <nav >
-          <CustomLink href="/" title="Home" className="mr-4" />
-          <CustomLink href="/about" title="About Me" className="mx-4" />
-          <CustomLink href="/projects" title="Projects" className="mx-4" />
-          <CustomLink href="/articles" title="Articles" className="mx-4" />
-          <CustomLink href="/contact_me" title="Contact Me" className="ml-4" />
-        </nav>
-
-
-        <nav className="flex justify-center items-center">
-          <motion.a
-            href="https://www.facebook.com/nadeesha.madusanka.5648/"
-            target={"_blank"}
-            whileHover={{ y: -3 }}
-            whileTap={{ scale: 0.9 }}
-            className="mr-3 w-7"
-          >
-            {mode === "dark" ? (
-              <FacebookIcon className={"fill-light"} />
-            ) : (
-              <FacebookIcon className={"fill-dark"} />
-            )}
-          </motion.a>
-          <motion.a
-            href="https://github.com/nadeeshaMadusanka99"
-            target={"_blank"}
-            whileHover={{ y: -3 }}
-            whileTap={{ scale: 0.9 }}
-            className="mx-3 w-7"
-          >
-            <GithubIcon />
-          </motion.a>
-          <motion.a
-            href="https://www.linkedin.com/in/nadeesha-madusanka-5454b2246/"
-            target={"_blank"}
-            whileHover={{ y: -3 }}
-            whileTap={{ scale: 0.9 }}
-            className="mx-3 w-6"
-          >
-            <LinkedInIcon />
-          </motion.a>
-          <motion.a
-            href="https://medium.com/@nadeeshamadusanka44"
-            target={"_blank"}
-            whileHover={{ y: -3 }}
-            whileTap={{ scale: 0.9 }}
-            className="ml-3 w-6"
-          >
-            {mode === "dark" ? (
-              <MediumIcon className={"fill-light"} />
-            ) : (
-              <MediumIcon className={"fill-dark"} />
-            )}
-          </motion.a>
+        <header className="w-full px-32 py-8 font-medium flex items-center  justify-between dark:text-light relative lg:px-16 md:px-12 sm:px-8 sm:pb-4">
           <button
-            onClick={() => setMode(mode === "light" ? "dark" : "light")}
-            className={`ml-4 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
-              }`}
+            className=" flex-col justify-center items-center hidden lg:flex fixed top-0 z-10 bg-light mt-5 rounded-full px-3 py-5 dark:bg-dark dark:text-light"
+            onClick={(e) => handleClick(e)}
           >
-            {mode === "dark" ? (
-              <SunIcon className={"fill-dark"} />
-            ) : (
-              <MoonIcon className={"fill-dark"} />
-            )}
-          </button>
-        </nav>
-      </div>
-      {isOpen ? (
-        <motion.div
-          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
-          ref={popUP}
-          animate={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 0.4 }}
-          className="min-w-[70vw] flex flex-col justify-center z-30 items-center fixed top-1/2 
-         left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
-        >
-          <nav className="flex items-center flex-col justify-center">
-            <CustomMobileLink toggle={handleClick} href="/" title="Home" />
-            <CustomMobileLink
-              toggle={handleClick}
-              href="/about"
-              title="About Me"
-            />
-            <CustomMobileLink
-              toggle={handleClick}
-              href="/projects"
-              title="Projects"
-            />
-            <CustomMobileLink
-              toggle={handleClick}
-              href="/articles"
-              title="Articles"
-            />
-            <CustomMobileLink
-              toggle={handleClick}
-              href="/contact_me"
-              title="Contact Me"
-            />
-          </nav>
-
-          <nav className="flex items-center justify-center flex-wrap mt-5">
-            <motion.a
-              href="https://www.facebook.com/nadeesha.madusanka.5648/"
-              target={"_blank"}
-              whileHover={{ y: -3 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-8 mx-5 sm:mx-1.5"
-            >
-              {mode === "dark" ? (
-                <FacebookIcon className={"fill-dark"} />
-              ) : (
-                <FacebookIcon className={"fill-light"} />
-              )}
-            </motion.a>
-            <motion.a
-              href="https://github.com/nadeeshaMadusanka99"
-              target={"_blank"}
-              whileHover={{ y: -3 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-8 mx-5 bg-light rounded-full dark:bg-dark sm:mx-1.5"
-            >
-              <GithubIcon />
-            </motion.a>
-            <motion.a
-              href="https://www.linkedin.com/in/nadeesha-madusanka-5454b2246/"
-              target={"_blank"}
-              whileHover={{ y: -3 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-8 mx-5 sm:mx-1.5"
-            >
-              <LinkedInIcon />
-            </motion.a>
-            <motion.a
-              href="https://medium.com/@nadeeshamadusanka44"
-              target={"_blank"}
-              whileHover={{ y: -3 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-8 mx-5 sm:mx-1.5"
-            >
-              {mode === "dark" ? (
-                <MediumIcon className={"fill-dark"} />
-              ) : (
-                <MediumIcon className={"fill-light"} />
-              )}
-            </motion.a>
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                setMode(mode === "light" ? "dark" : "light")
-              }}
-              className={`ml-4 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+            <span
+              className={`bg-dark dark:bg-light block transition-all duration-400 ease-out h-0.5 w-6 rounded-sm ${isOpen ? `rotate-45 translate-y-1` : `-translate-y-0.5`
                 }`}
+            ></span>
+            <span
+              className={`bg-dark dark:bg-light block transition-all duration-400 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? `opacity-0` : `opacity-100`
+                }`}
+            ></span>
+            <span
+              className={`bg-dark dark:bg-light block transition-all duration-400 ease-out h-0.5 w-6 rounded-sm ${isOpen ? `-rotate-45 -translate-y-1` : `translate-y-0.5`
+                }`}
+            ></span>
+          </button>
+
+          <div className="w-full flex justify-between items-center lg:hidden ">
+            <nav >
+              <CustomLink href="/" title="Home" className="mr-4" />
+              <CustomLink href="/about" title="About Me" className="mx-4" />
+              <CustomLink href="/projects" title="Projects" className="mx-4" />
+              <CustomLink href="/articles" title="Articles" className="mx-4" />
+              <CustomLink href="/contact_me" title="Contact Me" className="ml-4" />
+            </nav>
+
+
+            <nav className="flex justify-center items-center">
+              <motion.a
+                href="https://www.facebook.com/nadeesha.madusanka.5648/"
+                target={"_blank"}
+                whileHover={{ y: -3 }}
+                whileTap={{ scale: 0.9 }}
+                className="mr-3 w-7"
+              >
+                {mode === "dark" ? (
+                  <FacebookIcon className={"fill-light"} />
+                ) : (
+                  <FacebookIcon className={"fill-dark"} />
+                )}
+              </motion.a>
+              <motion.a
+                href="https://github.com/nadeeshaMadusanka99"
+                target={"_blank"}
+                whileHover={{ y: -3 }}
+                whileTap={{ scale: 0.9 }}
+                className="mx-3 w-7"
+              >
+                <GithubIcon />
+              </motion.a>
+              <motion.a
+                href="https://www.linkedin.com/in/nadeesha-madusanka-5454b2246/"
+                target={"_blank"}
+                whileHover={{ y: -3 }}
+                whileTap={{ scale: 0.9 }}
+                className="mx-3 w-6"
+              >
+                <LinkedInIcon />
+              </motion.a>
+              <motion.a
+                href="https://medium.com/@nadeeshamadusanka44"
+                target={"_blank"}
+                whileHover={{ y: -3 }}
+                whileTap={{ scale: 0.9 }}
+                className="ml-3 w-6"
+              >
+                {mode === "dark" ? (
+                  <MediumIcon className={"fill-light"} />
+                ) : (
+                  <MediumIcon className={"fill-dark"} />
+                )}
+              </motion.a>
+              <button
+                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                className={`ml-4 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+                  }`}
+              >
+                {mode === "dark" ? (
+                  <SunIcon className={"fill-dark"} />
+                ) : (
+                  <MoonIcon className={"fill-dark"} />
+                )}
+              </button>
+            </nav>
+          </div>
+          {isOpen ? (
+            <motion.div
+              initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+              ref={popUP}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.4 }}
+              className="min-w-[70vw] flex flex-col justify-center z-30 items-center fixed top-1/2 
+         left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
             >
-              {mode === "dark" ? (
-                <SunIcon className={"fill-dark lg:w-6 lg:h-6"} />
-              ) : (
-                <MoonIcon className={"fill-dark lg:w-6 lg:h-6"} />
-              )}
-            </button>
-          </nav>
-        </motion.div>
-      ) : null}
-    </header>
+              <nav className="flex items-center flex-col justify-center">
+                <CustomMobileLink toggle={handleClick} href="/" title="Home" />
+                <CustomMobileLink
+                  toggle={handleClick}
+                  href="/about"
+                  title="About Me"
+                />
+                <CustomMobileLink
+                  toggle={handleClick}
+                  href="/projects"
+                  title="Projects"
+                />
+                <CustomMobileLink
+                  toggle={handleClick}
+                  href="/articles"
+                  title="Articles"
+                />
+                <CustomMobileLink
+                  toggle={handleClick}
+                  href="/contact_me"
+                  title="Contact Me"
+                />
+              </nav>
+
+              <nav className="flex items-center justify-center flex-wrap mt-5">
+                <motion.a
+                  href="https://www.facebook.com/nadeesha.madusanka.5648/"
+                  target={"_blank"}
+                  whileHover={{ y: -3 }}
+                  whileTap={{ scale: 0.9 }}
+                  className="w-8 mx-5 sm:mx-1.5"
+                >
+                  {mode === "dark" ? (
+                    <FacebookIcon className={"fill-dark"} />
+                  ) : (
+                    <FacebookIcon className={"fill-light"} />
+                  )}
+                </motion.a>
+                <motion.a
+                  href="https://github.com/nadeeshaMadusanka99"
+                  target={"_blank"}
+                  whileHover={{ y: -3 }}
+                  whileTap={{ scale: 0.9 }}
+                  className="w-8 mx-5 bg-light rounded-full dark:bg-dark sm:mx-1.5"
+                >
+                  <GithubIcon />
+                </motion.a>
+                <motion.a
+                  href="https://www.linkedin.com/in/nadeesha-madusanka-5454b2246/"
+                  target={"_blank"}
+                  whileHover={{ y: -3 }}
+                  whileTap={{ scale: 0.9 }}
+                  className="w-8 mx-5 sm:mx-1.5"
+                >
+                  <LinkedInIcon />
+                </motion.a>
+                <motion.a
+                  href="https://medium.com/@nadeeshamadusanka44"
+                  target={"_blank"}
+                  whileHover={{ y: -3 }}
+                  whileTap={{ scale: 0.9 }}
+                  className="w-8 mx-5 sm:mx-1.5"
+                >
+                  {mode === "dark" ? (
+                    <MediumIcon className={"fill-dark"} />
+                  ) : (
+                    <MediumIcon className={"fill-light"} />
+                  )}
+                </motion.a>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setMode(mode === "light" ? "dark" : "light")
+                  }}
+                  className={`ml-4 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+                    }`}
+                >
+                  {mode === "dark" ? (
+                    <SunIcon className={"fill-dark lg:w-6 lg:h-6"} />
+                  ) : (
+                    <MoonIcon className={"fill-dark lg:w-6 lg:h-6"} />
+                  )}
+                </button>
+              </nav>
+            </motion.div>
+          ) : null}
+        </header>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
